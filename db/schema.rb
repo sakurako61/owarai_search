7e8ad804585da005f083b0804f669c9137478084
Sakurako61 <gstdv0719@gmail.com>
2026-03-29 12:27:23 +0900

    add:マイグレーション実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_03_24_071552) do
+ActiveRecord::Schema[7.2].define(version: 2026_03_29_025042) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,72 @@ ActiveRecord::Schema[7.2].define(version: 2026_03_24_071552) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "agencies", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comedians", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "agency_id"
+    t.index ["agency_id"], name: "index_comedians_on_agency_id"
+  end
+
+  create_table "group_comedians", force: :cascade do |t|
+    t.bigint "group_id", null: false
+    t.bigint "comedian_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["comedian_id"], name: "index_group_comedians_on_comedian_id"
+    t.index ["group_id"], name: "index_group_comedians_on_group_id"
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "performers", force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.bigint "group_id", null: false
+    t.bigint "comedian_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["comedian_id"], name: "index_performers_on_comedian_id"
+    t.index ["group_id"], name: "index_performers_on_group_id"
+    t.index ["post_id"], name: "index_performers_on_post_id"
+  end
+
+  create_table "places", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "post_code", limit: 8
+    t.string "address", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "live_name", null: false
+    t.string "discription"
+    t.datetime "open_date"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.datetime "ticket_start_date"
+    t.datetime "ticket_end_date"
+    t.money "price", scale: 2
+    t.text "live_url", null: false
+    t.bigint "user_id"
+    t.bigint "place_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["place_id"], name: "index_posts_on_place_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -58,4 +124,12 @@ ActiveRecord::Schema[7.2].define(version: 2026_03_24_071552) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comedians", "agencies"
+  add_foreign_key "group_comedians", "comedians"
+  add_foreign_key "group_comedians", "groups"
+  add_foreign_key "performers", "comedians"
+  add_foreign_key "performers", "groups"
+  add_foreign_key "performers", "posts"
+  add_foreign_key "posts", "places"
+  add_foreign_key "posts", "users"
 end

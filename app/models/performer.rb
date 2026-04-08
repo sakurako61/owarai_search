74f928e572792a71aca71d40fd415fb38264b9c1
Sakurako61 <gstdv0719@gmail.com>
2026-04-08 23:25:17 +0900

    fix:Post/Performer/Comedianモデルにransackable設定を追加

--- app/models/performer.rb
+++ app/models/performer.rb
@@ -2,4 +2,12 @@ class Performer < ApplicationRecord
   belongs_to :post
   belongs_to :group, optional: true
   belongs_to :comedian
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["comedian_id", "created_at", "group_id", "id", "post_id", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["comedian"]
+  end
 end
